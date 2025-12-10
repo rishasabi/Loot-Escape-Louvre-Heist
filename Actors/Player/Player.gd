7e22933f629e5f03501b57b70195a33387aec876
rishasabi <rishasabi05@gmail.com>
2025-12-10 18:57:40 +0530

    Level 1 Player created,added boundaries for room

--- Actors/Player/Player.gd
+++ Actors/Player/Player.gd
@@ -1,11 +1,53 @@
-extends Node2D
+extends CharacterBody2D
 
+@export var speed := 120
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var direction := Vector2.ZERO
 
+func _physics_process(delta):
+	direction = Vector2.ZERO
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Movement Inputs
+	if Input.is_action_pressed("ui_up"):
+		direction.y = -1
+	if Input.is_action_pressed("ui_down"):
+		direction.y = 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x = -1
+	if Input.is_action_pressed("ui_right"):
+		direction.x = 1
+
+	# Normalize diagonal movement
+	velocity = direction.normalized() * speed
+
+	move_and_slide()
+
+	_update_animation()
+func _update_animation():
+	var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+	if direction == Vector2.ZERO:
+		# Idle animations based on current walking animation
+		if anim.animation.ends_with("up"):
+			anim.animation = "idle_up"
+		elif anim.animation.ends_with("down"):
+			anim.animation = "idle_down"
+		elif anim.animation.ends_with("left"):
+			anim.animation = "idle_left"
+		elif anim.animation.ends_with("right"):
+			anim.animation = "idle_right"
+		anim.play()
+		return
+
+	# Walking animations
+	if direction.x > 0:
+		anim.animation = "walk_right"
+	elif direction.x < 0:
+		anim.animation = "walk_left"
+
+	if direction.y > 0:
+		anim.animation = "walk_down"
+	elif direction.y < 0:
+		anim.animation = "walk_up"
+
+	anim.play()
